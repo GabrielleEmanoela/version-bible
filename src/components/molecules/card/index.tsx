05f5ca77dfc57a626251eafd445a142b35466826
Gabrielle <biela.a09@gmail.com>
2024-06-30 19:25:10 -0300

    adjust card

--- src/components/molecules/card/index.tsx
+++ src/components/molecules/card/index.tsx
@@ -10,12 +10,17 @@ export type CardComponentType = {
 };
 
 export const CardComponent = (props: CardComponentType) => (
-  <Card>
+  <Card style={{ flex: 1, flexGrow: 20, justifyContent: "center", margin: 20 }}>
     <Card.Title {...props} />
     <Card.Content>
-      <Text variant="titleLarge">{props.titleCardContent}</Text>
+      <Text variant="titleLarge" style={{}}>
+        {props.titleCardContent}
+      </Text>
       <Text variant="bodyMedium">{props.bodyTitleContent}</Text>
     </Card.Content>
-    {/* <Card.Cover source={{ uri: props.imgUri }} /> */}
+    <Card.Cover
+      source={{ uri: "https://imgur.com/3OnGilN.png" }}
+      style={{ marginTop: 20, marginHorizontal: 20 }}
+    />
   </Card>
 );
